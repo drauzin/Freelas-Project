74fa0faafe4606a6bf7c78f626ae3de5a43a54e9
Drauzio Dominik <130460605+drauzin@users.noreply.github.com>
2025-06-03 19:15:29 -0300

    Update app.js

--- app.js
+++ app.js
@@ -6,36 +6,22 @@ const userRoutes = require('./routes/userRoutes');
 const offerRoutes = require('./routes/offerRoutes');
 
 const allowedOrigins = [
-  'http://localhost:8081'
- 
+  'http://localhost:8081',
+  'https://freelas-front.vercel.app' //
 ];
 
 app.use(cors({
-  origin: (origin, callback) => {
+  origin: function (origin, callback) {
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
     }
   },
-  credentials: true
+  credentials: true,
 }));
 
 app.use(express.json());
-
-// Middleware extra para responder preflight corretamente
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:8081'); // ou '*'
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  res.header('Access-Control-Allow-Credentials', 'true');
-
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(204);
-  }
-
-  next();
-});
 
 app.use('/api/users', userRoutes);
 app.use('/offers', offerRoutes);
