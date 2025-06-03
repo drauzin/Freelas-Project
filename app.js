d918c269bb4fd48843154ddc58d2769550c46a0f
Drauzio Dominik <130460605+drauzin@users.noreply.github.com>
2025-06-03 18:55:27 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
-
-
+const app = express();
 
 // Importar rotas
 const userRoutes = require('./routes/userRoutes');
 const offerRoutes = require('./routes/offerRoutes');
-
-const cors = require('cors');
-
 
 // CORS config
 app.use(cors({
@@ -18,16 +13,15 @@ app.use(cors({
   credentials: true
 }));
 
-
 // Middlewares
 app.use(express.json());
 
 // Rotas
+app.use('/api/users', userRoutes);  // <- Certifique-se de usar essa rota
 app.use('/offers', offerRoutes);
 
 app.get('/', (req, res) => {
   res.send('API FREELAS ONLINE.');
 });
-
 
 module.exports = app;
